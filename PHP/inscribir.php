322a546bbf7946439ebb4ac88ea935573235a7ad
benazk <benatezquerro128@gmail.com>
2024-01-26 12:09:53 +0100

    he creado la barra lateral y le he dado funcinalidad con js

---
+++ PHP/inscribir.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Datos recibidos:<br>
+Nombre: <?php echo $_GET["nombre"]; ?><br>
+Edad: <?php echo $_GET["edad"]; ?>
+
+</body>
+</html>
--- script.js
+++ script.js
@@ -22,3 +22,28 @@ function ocultarGaleria() {
         gris.style.display = "block";
     }
 }
+function mostrarBarra() {
+    var barra_lateral = document.querySelector(".barra_lateral");
+    var gris = document.getElementById("gris");
+
+    if (barra_lateral.style.display === "none") {
+        barra_lateral.style.display = "block";
+        gris.style.display = "block";
+    } else {
+        barra_lateral.style.display = "none";
+        gris.style.display = "none";
+    }
+}
+function ocultarBarra() {
+    var barra_lateral = document.querySelector(".barra_lateral");
+    var gris = document.getElementById("gris");
+
+    if (barra_lateral.style.display === "block") {
+        barra_lateral.style.display = "none";
+        gris.style.display = "none";
+    } else {
+        barra_lateral.style.display = "block";
+        gris.style.display = "block";
+    }
+}
+
